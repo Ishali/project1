1c50074a2abcda1697e27e5b2a83cedb688c1649
Ishali <ishalijain9@gmail.com>
2022-09-16 12:41:21 +0530

    message2

--- lambda_stack/lambda_stack.py
+++ lambda_stack/lambda_stack.py
@@ -19,12 +19,13 @@ class LambdaStack(Stack):
 
         ## Creating a lambda function for the Generic Logger Lambda Function
         generic_layer = lambda_.LayerVersion(self, f"{config['lambda_fn']['stack_name']}-layer",
-                                   layer_version_name=config['lambda_fn']['lambda_layers']['name'],
-                                   code=lambda_.AssetCode(config['lambda_fn']['lambda_layers']['path']),
-                                   compatible_runtimes=[lambda_.Runtime.PYTHON_3_9],
-                                   compatible_architectures=[lambda_.Architecture.X86_64])
-        ver = generic_layer.current_version
-        print("version of lambda layer" , ver)
+                                             layer_version_name=config['lambda_fn']['lambda_layers']['name'],
+                                             code=lambda_.AssetCode(config['lambda_fn']['lambda_layers']['path']),
+                                             removal_policy=aws_cdk.RemovalPolicy.RETAIN,
+                                             compatible_runtimes=[lambda_.Runtime.PYTHON_3_9],
+                                             compatible_architectures=[lambda_.Architecture.X86_64])
+        #ver = generic_layer.current_version
+        #print("version of lambda layer" , ver)
         # Lambda-Role
         generic_lambda_role = aws_iam.Role(self, f"{config['lambda_fn']['stack_name']}-lambda-role",
                                    assumed_by=aws_iam.ServicePrincipal('lambda.amazonaws.com'),
